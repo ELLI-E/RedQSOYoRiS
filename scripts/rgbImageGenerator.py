9f792d60f998e25c49d8d4db274db8cec083c440
Ellie Saga <elliesaga03@gmail.com>
2024-11-08 12:18:11 +0000

    Made a generateImages function in the rgbImageGenerator script

--- scripts/rgbImageGenerator.py
+++ scripts/rgbImageGenerator.py
@@ -5,19 +5,22 @@ from astropy.visualization import make_lupton_rgb
 import astropy.io.fits as pyfits
 from matplotlib import pyplot as plt
 matplotlib.use('Agg')
-dataAddress = r"data/images"
-imageAddress = r"rgbimages"
-contamCSV = r"data/contaminationCheck.csv"
-objects,contaminated = np.loadtxt(contamCSV,unpack=True,delimiter=",",skiprows=1,dtype=str)
+def generateImages(objects,imageAddress,dataAddress,contaminated=[]):
+    #commented out old addresses, leaving for reference
+    """dataAddress = r"data/images"
+    imageAddress = r"rgbimages"
+    contamCSV = r"data/contaminationCheck.csv"
+    objects,contaminated = np.loadtxt(contamCSV,unpack=True,delimiter=",",skiprows=1,dtype=str)"""
 
-for i, object in enumerate(objects):
-    if contaminated[i] == " 1":
-        continue
-    else:
-        #make image for each object
-        R = pyfits.open(f"{dataAddress}/{object}/{object}_HSC-I.fits")[1].data
-        G = pyfits.open(f"{dataAddress}/{object}/{object}_HSC-R.fits")[1].data
-        B = pyfits.open(f"{dataAddress}/{object}/{object}_HSC-G.fits")[1].data
-        image = make_lupton_rgb(R,G,B,stretch = 0.5,Q=10)
-        plt.imshow(image)
-        plt.savefig(f"{imageAddress}/{object}.png")
+    for i, object in enumerate(objects):
+        if len(contaminated) != 0: #if length of list is the default 0, do not check for contamination
+            if contaminated[i] == " 1":
+                continue
+        else:
+            #make image for each object
+            R = pyfits.open(f"{dataAddress}/{object}/{object}_HSC-I.fits")[1].data
+            G = pyfits.open(f"{dataAddress}/{object}/{object}_HSC-R.fits")[1].data
+            B = pyfits.open(f"{dataAddress}/{object}/{object}_HSC-G.fits")[1].data
+            image = make_lupton_rgb(R,G,B,stretch = 0.5,Q=10)
+            plt.imshow(image)
+            plt.savefig(f"{imageAddress}/{object}.png")
